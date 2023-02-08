a34f4f0ab55126cd07e87e9369f9624c94b38156
FernandoRodCor <frodr178@ford.com>
2023-02-08 17:50:03 -0600

    resized image tutorial, imutils aspect ratio

--- opencv.py
+++ opencv.py
@@ -10,4 +10,25 @@ print("width={}, height={}, depth={}".format(w, h, d))
 # display the image to our screen -- we will need to click the window
 # open by OpenCV and press a key on our keyboard to continue execution
 cv2.imshow("Image", image)
+#cv2.waitKey(0)
+
+(B,G,R) = image[100,60]
+print("R={}, G={}, B={}".format(R,G,B))
+
+roi = image[60:160, 320:420]
+cv2.imshow("ROI", roi)
+#cv2.waitKey(0)
+
+
+""" 
+#new aspect ratio based on width = 300 px
+r = 300.0/w
+newDim = (300, int(h*r))
+resized = cv2.resize(image, newDim)
+cv2.imshow("Aspect ratio new size", resized)
+cv2.waitKey(0) 
+"""
+
+resized = imutils.resize(image, width = 300)
+cv2.imshow("Imutils resize", resized)
 cv2.waitKey(0)
