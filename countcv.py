8743d007fa44aa9c782a57dc5b4c517a771c31f0
FernandoRodCor <rodriguezcortes.fer@gmail.com>
2023-02-13 08:41:54 -0600

    deleted a block comment and added mask operation

--- countcv.py
+++ countcv.py
@@ -56,18 +56,10 @@ cv2.putText(output, text, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7,	(240, 0, 159
 cv2.imshow("Contours", output)
 cv2.waitKey(0)
 
-""" 
-# find contours (i.e., outlines) of the foreground objects in the
-# thresholded image
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-output = image.copy()
-# loop over the contours
-for c in cnts:
-	# draw each contour on the output image with a 3px thick purple
-	# outline, then display the output contours one at a time
-	cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
-	cv2.imshow("Contours", output)
-	cv2.waitKey(0)
-"""
+# a typical operation we may want to apply is to take our mask and
+# apply a bitwise AND to our input image, keeping only the masked
+# regions
+mask = thresh.copy()
+output = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
