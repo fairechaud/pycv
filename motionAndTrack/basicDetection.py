d59d3cae22818a0fe5f602c03f9a603c1a2c852a
FernandoRodCor <rodriguezcortes.fer@gmail.com>
2023-02-17 13:08:22 -0600

    Working example with testVideo.mov

--- motionAndTrack/basicDetection.py
+++ motionAndTrack/basicDetection.py
@@ -1,14 +1,14 @@
 # import the necessary packages
+import imutils
 from imutils.video import VideoStream
 import argparse
 import datetime
-import imutils
 import time
 import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area", type=int, default=700, help="minimum area size")
 args = vars(ap.parse_args())
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
@@ -31,7 +31,7 @@ while True:
 	if frame is None:
 		break
 	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=500)
+	frame = imutils.resize(frame, width=250)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 	# if the first frame is None, initialize it
